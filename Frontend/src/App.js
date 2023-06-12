a6d0f7edb0f232933c278f4ba63a088f9decb169
andrescaro16 <julicaro2003@gmail.com>
2023-06-12 10:03:27 -0500

    Login page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,32 @@
-import { getTowns } from "./Api/api";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "./Pages/Login/Login";
 
 function App() {
 
-  (async () => {
-    const res = await getTowns();
-    if(!res.loading){
-      console.log(res);
-    }
-  })();
-
   return (
-    <></>
+    <>
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<h1>Home</h1>} />
+
+          <Route path="/add-trip" element={<h1>Add Trip</h1>} />
+
+          <Route path="/viatic-salary" element={<h1>Viatic and Salary</h1>} />
+
+          <Route path="/trip-history" element={<h1>History</h1>} />
+          <Route path="/edit-trip/:id" element={<h1>Edit Trip</h1>} />
+
+          <Route path="/towns" element={<h1>Towns</h1>} />
+          <Route path="/add-town" element={<h1>Add Town</h1>} />
+          <Route path="/edit-town/:id" element={<h1>Edit Town</h1>} />
+
+        </Routes>
+    
+      </ BrowserRouter>
+    </>
   );
 }
 
---
+++ Frontend/src/Pages/Login/Login.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Lottie from 'lottie-react';
+import truckAnimation from '../../Assets/Animations/truckAnimation.json';
+import './Login.css';
+
+function Login() {
+
+	const [showPassword, setShowPassword] = useState(false);
+	const [error, setError] = useState(false);
+
+	const navigate = useNavigate();
+
+	function togglePassword(event) {
+		event.preventDefault();
+		setShowPassword(!showPassword);
+	}
+
+	function onSubmit(event) {
+		event.preventDefault();
+		if(event.target[0].value === `${process.env.REACT_APP_USERNAME}` && event.target[1].value === `${process.env.REACT_APP_PASSWORD}`) {
+			navigate('/home');
+		} else {
+			setError(true);
+		}
+	}
+
+	return (
+		<div className='login-container'>
+
+			<Lottie animationData={truckAnimation} className='login-truck-animation' />
+			<h1 className='login-title'>Inicio de sesión</h1>
+
+			<form className='login-form' onSubmit={onSubmit}>
+
+				<div className='login-content-container'>
+					<label className='login-label-username'>Nombre de usuario</label>
+					<input className='login-input-username' type='text' placeholder='Nombre de usuario'/>
+				</div>
+
+				<div className='login-content-container'>
+					<label className='login-label-password'>Contraseña</label>
+					<input className='login-input-password' type={showPassword ? 'text' : 'password'} placeholder='Contraseña'/>
+					<button type='button' className='login-toggle-button' onClick={togglePassword} >
+						{showPassword ? 'Ocultar' : 'Mostrar'}
+					</button>
+				</div>
+
+				{error && <p className='login-error'>Usuario o contraseña incorrectos</p>}
+
+				<button type="submit" className="login-button"> Iniciar sesión </button>
+
+			</form>
+
+		</div>
+	)
+}
+
+export { Login };
